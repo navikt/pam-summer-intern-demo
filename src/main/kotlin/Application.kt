54073e3691b78f159378db7eb32271de0fd522ef
Martin Wangen-Eriksen <martinwe@Martins-MacBook-Pro.local>
2020-06-24 10:41:19 +0200

    make demo

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.util.concurrent.atomic.AtomicLong
+
+@SpringBootApplication
+class Application {
+
+    fun main(args: Array<String>) {
+        SpringApplication.run(Application::class.java, *args)
+    }
+
+}
