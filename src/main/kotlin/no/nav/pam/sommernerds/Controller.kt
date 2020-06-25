407cb7b0ddfd529693ef10cd7a88401eda02e5df
catriont <catriona.torklep@gmail.com>
2020-06-25 12:32:12 +0200

    fix spring boot run error

---
+++ src/main/kotlin/no/nav/pam/sommernerds/Controller.kt
@@ -0,0 +1,13 @@
+package no.nav.pam.sommernerds
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class testController {
+    @GetMapping(value = ["/hello"]) //@RequestMapping(value="/",method=RequestMethod.GET)
+    fun hello(): String {
+        return "Hello World!!"
+    }
+}
+
