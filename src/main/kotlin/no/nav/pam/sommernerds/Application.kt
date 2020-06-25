81a5764728e2ff2331a32339e410b1cb05fbfa6c
catriont <catriona.torklep@gmail.com>
2020-06-25 12:32:12 +0200

    fix spring boot run error

---
+++ src/main/kotlin/no/nav/pam/sommernerds/Application.kt
@@ -0,0 +1,12 @@
+package no.nav.pam.sommernerds
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+    fun main(args: Array<String>) {
+        runApplication<Application>(*args)
+    }
--- src/main/kotlin/test.kt
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class BlogApplication
-
-fun main(args: Array<String>) {
-    runApplication<BlogApplication>(*args)
-}
