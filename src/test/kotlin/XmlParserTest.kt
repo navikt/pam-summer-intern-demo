8495ea9479e4bc098929fd8df03714316501fdae
catriont <catriona.torklep@gmail.com>
2020-06-26 16:01:40 +0200

    add XmlParser test

--- src/test/kotlin/XmlParserTest.kt
+++ src/test/kotlin/XmlParserTest.kt
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.Test
 
 class XmlParserTest {
     @Test
-    fun `verifiser godkjent bedrift`(){
+    fun `verifiser godkjent bedrift`() {
         val noe = javaClass.getResource("/TestData.xml").readText()
         assertEquals(findBedrift(972094722, noe), "Godkjent med ansatte")
     }
 
     @Test
-    fun `verifiser ukjent bedrift`(){
+    fun `verifiser ukjent bedrift`() {
         val noe = javaClass.getResource("/TestData.xml").readText()
         assertEquals(findBedrift(69420, noe), "Ikke renholdsbedrift")
     }
 
     @Test
-    fun `verifiser ulovlig bedrift`(){
+    fun `verifiser ulovlig bedrift`() {
         val noe = javaClass.getResource("/TestData.xml").readText()
         assertEquals(findBedrift(958942192, noe), "Ikke godkjent")
     }
