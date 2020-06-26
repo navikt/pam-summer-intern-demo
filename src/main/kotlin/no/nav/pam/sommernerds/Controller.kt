160c30492352f93ce6605f4aab9bd06b7131ade6
catriont <catriona.torklep@gmail.com>
2020-06-26 15:56:27 +0200

    add XmlParser test

--- src/main/kotlin/no/nav/pam/sommernerds/Controller.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/Controller.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class testController {
+class Controller {
 
     @GetMapping(value = ["/hello"]) // @RequestMapping(value="/",method=RequestMethod.GET)
     fun hello(): String {
--- src/main/kotlin/no/nav/pam/sommernerds/XmlParser.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/XmlParser.kt
@@ -1,69 +1,59 @@
 package no.nav.pam.sommernerds
 
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import java.net.URL
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
-import com.fasterxml.jackson.annotation.Nulls
-import com.fasterxml.jackson.annotation.JsonSetter
-
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.net.URL
 
 @JacksonXmlRootElement(localName = "ArrayOfRenholdsvirksomhet")
 data class rot(
-        @JacksonXmlProperty(isAttribute = false, localName = "Renholdsvirksomhet")
-        val bedrifter: List<Renhold>
+    @JacksonXmlProperty(isAttribute = false, localName = "Renholdsvirksomhet")
+    val bedrifter: List<Renhold>
 )
-
-
 
 data class Renhold(
-        @JacksonXmlProperty(isAttribute = false, localName = "Organisasjonsnummer")
-        val orgnr: Int,
+    @JacksonXmlProperty(isAttribute = false, localName = "Organisasjonsnummer")
+    val orgnr: Int,
 
-        @JacksonXmlProperty(isAttribute = false, localName = "Status")
-        val status: String,
+    @JacksonXmlProperty(isAttribute = false, localName = "Status")
+    val status: String,
 
-        @JacksonXmlProperty(isAttribute = false, localName = "Underavdelinger")
-        val underavdeling: Underavdeling?
+    @JacksonXmlProperty(isAttribute = false, localName = "Underavdelinger")
+    val underavdeling: Underavdeling?
 
 )
 
-
 data class Underavdeling(
-        @JacksonXmlProperty(isAttribute = false, localName = "Avdeling")
-        val avdelinger: List<Avdeling>
+    @JacksonXmlProperty(isAttribute = false, localName = "Avdeling")
+    val avdelinger: List<Avdeling>
 )
 
 data class Avdeling(
-        @JacksonXmlProperty(isAttribute = false, localName = "Organisasjonsnummer")
-        val avdorgnr: Int
-
+    @JacksonXmlProperty(isAttribute = false, localName = "Organisasjonsnummer")
+    val avdorgnr: Int
 
 )
 
+fun findBedrift(orgnummer: Int, xml: String): String {
+    val mapper = XmlMapper(
+        JacksonXmlModule().apply {
+            setDefaultUseWrapper(false)
+        }
+    ).registerKotlinModule()
+        .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-fun findBedrift(orgnummer: Int): String {
-    val mapper = XmlMapper(JacksonXmlModule().apply {
-        setDefaultUseWrapper(false)
-    }).registerKotlinModule()
-            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            
-
-
-    //val rsc = Renhold::class.java.getResource("/test2.xml")
     val url = URL("https://www.arbeidstilsynet.no/opendata/renhold.xml")
-    //val test = mapper.readValue(url, rot::class.java).bedrifter.filter { it.orgnr == orgnummer }
-    val test = mapper.readValue(url, rot::class.java).bedrifter
-    for(bedrift in test) {
+    // val test = mapper.readValue(url, rot::class.java).bedrifter.filter { it.orgnr == orgnummer }
+    val test = mapper.readValue(xml, rot::class.java).bedrifter
+    for (bedrift in test) {
         if (bedrift.orgnr == orgnummer) {
             return bedrift.status
-        }
-        else {
+        } else {
             bedrift.underavdeling?.avdelinger?.forEach() {
                 if (it.avdorgnr == orgnummer) {
                     return bedrift.status
@@ -71,9 +61,9 @@ fun findBedrift(orgnummer: Int): String {
             }
         }
     }
-    return "nah"
+    return "Ikke renholdsbedrift"
 }
 
 fun main(args: Array<String>) {
-    println(findBedrift(972094722))
+    println(findBedrift(952283200, "/test2.xml"))
 }
