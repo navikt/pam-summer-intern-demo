2e3c9d795e5562b7006684584b77b0ab020e9d98
Martin Wangen-Eriksen <martinwe@Martins-MacBook-Pro.local>
2020-06-30 15:23:09 +0200

    add files to deploy from another project

--- src/test/kotlin/XmlParserTest.kt
+++ src/test/kotlin/XmlParserTest.kt
@@ -5,36 +5,32 @@ import org.junit.jupiter.api.Test
 class XmlParserTest {
     @Test
     fun `verifiser godkjent bedrift`() {
-        assertEquals(findBedrift(972094722, "/renhold.xml"), "Godkjent med ansatte")
+        assertEquals(findBedrift("972094722", "/renhold.xml"), "Godkjent med ansatte")
     }
 
     @Test
     fun `verifiser ukjent bedrift`() {
-        assertEquals(findBedrift(69420, "/renhold.xml"), "Ikke renholdsbedrift")
+        assertEquals(findBedrift("69420", "/renhold.xml"), "Ikke renholdsbedrift")
     }
 
     @Test
     fun `verifiser ulovlig bedrift`() {
-        assertEquals(findBedrift(958942192, "/renhold.xml"), "Ikke godkjent")
+        assertEquals(findBedrift("958942192", "/renhold.xml"), "Ikke godkjent")
     }
 
     @Test
     fun `verifiser godkjent uten ansatte bedrift`() {
-        assertEquals(findBedrift(979952562, "/renhold.xml"), "Godkjent uten ansatte")
+        assertEquals(findBedrift("979952562", "/renhold.xml"), "Godkjent uten ansatte")
     }
 
     @Test
     fun `verifiser HMS-kort bedrift`() {
-        assertEquals(findBedrift(996168484, "/renhold.xml"), "Under HMS-kortbestilling")
+        assertEquals(findBedrift("996168484", "/renhold.xml"), "Under HMS-kortbestilling")
     }
 
     @Test
     fun `verifiser under behandling bedrift`() {
-        assertEquals(findBedrift(993096202, "/renhold.xml"), "Under behandling")
+        assertEquals(findBedrift("993096202", "/renhold.xml"), "Under behandling")
     }
 
-    @Test
-    fun `math`() {
-        assertEquals(2, 2)
-    }
 }
