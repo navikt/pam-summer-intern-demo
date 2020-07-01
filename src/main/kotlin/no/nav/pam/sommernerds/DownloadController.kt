f4227ef21aeeb61ff389e6cdfdda542f5014d849
Martin Wangen-Eriksen <martin.wangen-eriksen@nav.no>
2020-07-01 12:59:36 +0200

    delete function in dowloadcontroller

--- src/main/kotlin/no/nav/pam/sommernerds/DownloadController.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/DownloadController.kt
@@ -1,21 +1,10 @@
 package no.nav.pam.sommernerds
 
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClientBuilder
-import org.springframework.core.io.ByteArrayResource
-import org.springframework.core.io.InputStreamResource
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStream
 import java.net.URL
-import java.net.http.HttpClient
-import java.nio.file.Files
-import java.nio.file.Path
-import javax.annotation.Resource
+
 
 
 fun download(link: String, path: String) {
@@ -26,25 +15,6 @@ fun download(link: String, path: String) {
     }
 }
 
-/*@RequestMapping(path = ["/{download}"],  method = [RequestMethod.GET])
-fun downloadSpring(path : Path) : ResponseEntity<Resource> {
-    val url = "https://www.arbeidstilsynet.no/opendata/renhold.xml";
-    val httpClient = HttpClientBuilder.create().build();
-    val request = HttpGet(url)
-
-    val response = httpClient.execute(request);
-    if (response.statusLine.statusCode == 200){
-        val resource = ByteArrayResource(Files.readAllBytes(path))
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentLength(file.length())
-                .contentType(MediaType.parseMediaType("hahaha"))
-                .body(response.entity.content)
-    }else{
-        return ResponseEntity.notFound();
-    }
-
-}*/
 
 
 fun main(args: Array<String>) {
