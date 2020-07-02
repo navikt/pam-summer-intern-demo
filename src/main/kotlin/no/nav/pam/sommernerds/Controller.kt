a0592456e3afd226018430c87c7abe4b8b00c7b6
Yong Bin Kwon <yongbinkwon97@gmail.com>
2020-07-02 13:21:08 +0200

    try to return bedriftstatus when given orgnummer

--- src/main/kotlin/no/nav/pam/sommernerds/Controller.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/Controller.kt
@@ -3,15 +3,16 @@ package no.nav.pam.sommernerds
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class Controller {
 
-    @GetMapping(value = ["/hello"]) // @RequestMapping(value="/",method=RequestMethod.GET)
-    fun hello(): String {
+    @GetMapping(value = ["/{orgnummer}"]) // @RequestMapping(value="/",method=RequestMethod.GET)
+    fun hello(@PathVariable("orgnummer") nr: String): String {
 
-        return "Hello World!!"
+        return findBedrift(nr, "/renhold.xml")
     }
 
     @GetMapping("/isAlive")
