964b2bdbd2e9cab932f9ec04f7776072beeeaef9
Martin Wangen-Eriksen <martin.wangen-eriksen@nav.no>
2020-07-02 16:27:27 +0200

    add scheduler

--- src/main/kotlin/no/nav/pam/sommernerds/DownloadController.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/DownloadController.kt
@@ -1,12 +1,13 @@
 package no.nav.pam.sommernerds
 
 
+import org.springframework.scheduling.annotation.Scheduled
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URL
 
 
-
+@Scheduled(cron = "0 0 5 * * *", zone = "Europe/Oslo" )
 fun download(link: String, path: String) {
     URL(link).openStream().use { input ->
         FileOutputStream(File(path)).use { output ->
