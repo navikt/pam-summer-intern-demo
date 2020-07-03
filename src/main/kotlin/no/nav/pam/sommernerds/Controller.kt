82e3731ad4655e0578ff24d6ebe67d040490ae77
Yong Bin Kwon <yongbinkwon97@gmail.com>
2020-07-03 10:31:23 +0200

    try using beans

--- src/main/kotlin/no/nav/pam/sommernerds/Controller.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/Controller.kt
@@ -1,5 +1,6 @@
 package no.nav.pam.sommernerds
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController
 class Controller {
 
     @GetMapping(value = ["/{orgnummer}"]) // @RequestMapping(value="/",method=RequestMethod.GET)
-    fun hello(@PathVariable("orgnummer") nr: String): String {
-
-        return findBedrift(nr, "/renhold.xml")
+    fun hello(@PathVariable("orgnummer") nr: String): String? {
+        var actx = AnnotationConfigApplicationContext(XmlParser::class.java)
+        var dataContainer = actx.getBean(DataContainer::class.java)
+        return dataContainer.dictionary[nr]
     }
 
     @GetMapping("/isAlive")
