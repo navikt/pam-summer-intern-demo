c651f00c3804e7a1563e2899144e369ed746b148
Yong Bin Kwon <yongbinkwon97@gmail.com>
2020-07-07 08:59:49 +0200

    add component scan

--- src/main/kotlin/no/nav/pam/sommernerds/Application.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/Application.kt
@@ -2,10 +2,11 @@ package no.nav.pam.sommernerds
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
+@ComponentScan(basePackages = arrayOf("no.nav.pam.sommernerds"))
 class Application
 
 fun main(args: Array<String>) {
