20596852cd8fc8e37f3cf54efce5dce52b7f2498
catriont <catriona.torklep@gmail.com>
2020-07-08 09:46:16 +0200

    Remove redundant declaration

--- src/main/kotlin/no/nav/pam/sommernerds/Application.kt
+++ src/main/kotlin/no/nav/pam/sommernerds/Application.kt
@@ -2,9 +2,7 @@ package no.nav.pam.sommernerds
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
-@ComponentScan("no.nav.pam.sommernerds")
 @SpringBootApplication
 class Application
 
